d104682a9276af43ad63a9812e9a53897d499456
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2014-04-26 11:17:26 +0100

    ASoC: wm_adsp: Remove compilation errors when no CONFIG_OF
    Change-Id: I87eb1800233317438e5c5efd725a428bbaec8a13
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -2216,6 +2216,7 @@ err:
 }
 EXPORT_SYMBOL_GPL(wm_adsp2_event);
 
+#ifdef CONFIG_OF
 static int wm_adsp_of_parse_caps(struct wm_adsp *adsp,
 				 struct device_node *np,
 				 struct wm_adsp_fw_defs *fw)
@@ -2361,6 +2362,12 @@ static int wm_adsp_of_parse_adsp(struct wm_adsp *adsp)
 
 	return wm_adsp_of_parse_firmware(adsp, core);
 }
+#else
+static inline int wm_adsp_of_parse_adsp(struct wm_adsp *adsp)
+{
+	return 0;
+}
+#endif
 
 int wm_adsp2_init(struct wm_adsp *adsp, bool dvfs)
 {
