af9de0652545db62155d31334002bc868ac75085
Samreen Nilofer <samreen.nilofer@intel.com>
2014-08-02 12:42:46 +0530

    UAPI: Add tlv.h to uapi headers
    BZ: 0
    
    split the tlv.h header to add the required data in the uapi tlv.h
    
    Change-Id: I38c0af0766ad6aac30850f1b0b49c9fb75126c81
    Signed-off-by: Samreen Nilofer <samreen.nilofer@intel.com>
    Reviewed-on: https://android.intel.com/224594
    Reviewed-by: Kp, Jeeja <jeeja.kp@intel.com>
    Tested-by: Kp, Jeeja <jeeja.kp@intel.com>

--- include/sound/tlv.h
+++ include/sound/tlv.h
@@ -22,6 +22,8 @@
  *
  */
 
+#include <uapi/sound/tlv.h>
+
 /*
  * TLV structure is right behind the struct snd_ctl_tlv:
  *   unsigned int type  	- see SNDRV_CTL_TLVT_*
@@ -31,32 +33,10 @@
  *                       ~(sizeof(unsigned int) - 1)) ....
  */
 
-#define SNDRV_CTL_TLVT_CONTAINER 0	/* one level down - group of TLVs */
-#define SNDRV_CTL_TLVT_DB_SCALE	1       /* dB scale */
-#define SNDRV_CTL_TLVT_DB_LINEAR 2	/* linear volume */
-#define SNDRV_CTL_TLVT_DB_RANGE 3	/* dB range container */
-#define SNDRV_CTL_TLVT_DB_MINMAX 4	/* dB scale with min/max */
-#define SNDRV_CTL_TLVT_DB_MINMAX_MUTE 5	/* dB scale with min/max with mute */
-
-#define TLV_ITEM(type, ...) \
-	(type), TLV_LENGTH(__VA_ARGS__), __VA_ARGS__
-#define TLV_LENGTH(...) \
-	((unsigned int)sizeof((const unsigned int[]) { __VA_ARGS__ }))
-
 #define TLV_CONTAINER_ITEM(...) \
 	TLV_ITEM(SNDRV_CTL_TLVT_CONTAINER, __VA_ARGS__)
 #define DECLARE_TLV_CONTAINER(name, ...) \
 	unsigned int name[] = { TLV_CONTAINER_ITEM(__VA_ARGS__) }
-
-#define TLV_DB_SCALE_MASK	0xffff
-#define TLV_DB_SCALE_MUTE	0x10000
-#define TLV_DB_SCALE_ITEM(min, step, mute)			\
-	TLV_ITEM(SNDRV_CTL_TLVT_DB_SCALE,			\
-		 (min),					\
-		 ((step) & TLV_DB_SCALE_MASK) |		\
-			((mute) ? TLV_DB_SCALE_MUTE : 0))
-#define DECLARE_TLV_DB_SCALE(name, min, step, mute) \
-	unsigned int name[] = { TLV_DB_SCALE_ITEM(min, step, mute) }
 
 /* dB scale specified with min/max values instead of step */
 #define TLV_DB_MINMAX_ITEM(min_dB, max_dB)			\
