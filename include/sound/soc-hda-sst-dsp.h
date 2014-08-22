1ef83323af7af704bbc427672f35faa92c17dc2e
Jeeja KP <jeeja.kp@intel.com>
2014-08-22 14:32:38 +0530

    Audio: HDA-DSP registers and DSP core functions
    BZ:NONE
    
    This patch includes
    - HDA DSP core functions
         * DSP core reset
         * DSP core enable/disable
         * DSP core power up/ power down
         * Macros for read/write DSP registers
         * Interrupt handler
         * DSP library interface function - dsp init() & dsp_free()
    - defines ADSP registers space
    
    Change-Id: I8ad2ef385eaf795ac3cfe8f6d88f3a3e33e31b14
    Signed-off-by: Jeeja KP <jeeja.kp@intel.com>
    Reviewed-on: https://android.intel.com/234149

---
+++ include/sound/soc-hda-sst-dsp.h
@@ -0,0 +1,425 @@
+/*Add header */
+#ifndef __HDA_SST_DSP_H__
+#define __HDA_SST_DSP_H__
+
+#include <linux/spinlock.h>
+#include <sound/memalloc.h>
+
+#define sst_writel_andor(ctx, reg, mask_and, mask_or) \
+	sst_writel_traced( \
+	ctx, \
+	reg, \
+	(sst_readl_traced(ctx, (reg)) & (mask_and) | (mask_or)))
+
+#define sst_readb(ctx, reg) \
+	sst_readb_traced(ctx, HDA_ADSP_REG_##reg)
+#define sst_readw(ctx, reg) \
+	sst_readw_traced(ctx, HDA_ADSP_REG_##reg)
+#define sst_readl(ctx, reg) \
+	sst_readl_traced(ctx, HDA_ADSP_REG_##reg)
+#define sst_readl_alt(ctx, reg) \
+	sst_readl_traced(ctx, reg)
+
+#define sst_writeb(ctx, reg, value) \
+	sst_writeb_traced(ctx, HDA_ADSP_REG_##reg, (value))
+#define sst_writew(ctx, reg, value) \
+	sst_writew_traced(ctx, HDA_ADSP_REG_##reg, (value))
+#define sst_writel(ctx, reg, value) \
+	sst_writel_traced(ctx, HDA_ADSP_REG_##reg, (value))
+
+#if 0
+#define sst_writel_andor(ctx, reg, mask_and, mask_or) \
+	sst_writel( \
+		ctx, \
+		reg, \
+		(sst_readl(ctx, (reg)) & (mask_and) | (mask_or)))
+
+#define sst_readb(ctx, reg) \
+	readb((ctx)->mmio_base + HDA_ADSP_REG_##reg);
+#define sst_readw(ctx, reg) \
+	readw((ctx)->mmio_base + HDA_ADSP_REG_##reg)
+#define sst_readl(ctx, reg) \
+	readl((ctx)->mmio_base + HDA_ADSP_REG_##reg)
+#define sst_readl_alt(ctx, reg) \
+	readl((ctx)->mmio_base + reg)
+
+#define sst_writeb(ctx, reg, value) \
+	writeb(value, (ctx)->mmio_base + HDA_ADSP_REG_##reg)
+#define sst_writew(ctx, reg, value) \
+	writew(value, (ctx)->mmio_base + HDA_ADSP_REG_##reg)
+#define sst_writel(ctx, reg, value) \
+	writel(value, (ctx)->mmio_base + HDA_ADSP_REG_##reg)
+#endif
+
+#define sst_updatel(ctx, reg, mask, value) \
+	sst_updatel_bits(ctx, HDA_ADSP_REG_##reg, reg##_##mask, (value))
+
+#define sst_updatel_locked(ctx, reg, mask, value) \
+	sst_updatel_bits_locked(ctx, HDA_ADSP_REG_##reg, \
+		reg##_##mask, (value))
+
+/* Intel® HD Audio General DSP Registers */
+#define HDA_ADSP_GEN_BASE		0x0
+#define HDA_ADSP_REG_ADSPCS		(HDA_ADSP_GEN_BASE + 0x04)
+#define HDA_ADSP_REG_ADSPIC		(HDA_ADSP_GEN_BASE + 0x08)
+#define HDA_ADSP_REG_ADSPIS		(HDA_ADSP_GEN_BASE + 0x0C)
+#define HDA_ADSP_REG_ADSPIC2		(HDA_ADSP_GEN_BASE + 0x10)
+#define HDA_ADSP_REG_ADSPIS2		(HDA_ADSP_GEN_BASE + 0x14)
+
+/* Intel® HD Audio Inter-Processor Communication Registers */
+#define HDA_ADSP_IPC_BASE		0x40
+#define HDA_ADSP_REG_HIPCT		(HDA_ADSP_IPC_BASE + 0x00)
+#define HDA_ADSP_REG_HIPCTE		(HDA_ADSP_IPC_BASE + 0x04)
+#define HDA_ADSP_REG_HIPCI		(HDA_ADSP_IPC_BASE + 0x08)
+#define HDA_ADSP_REG_HIPCIE		(HDA_ADSP_IPC_BASE + 0x0C)
+#define HDA_ADSP_REG_HIPCCTL		(HDA_ADSP_IPC_BASE + 0x10)
+
+/*  HIPCI */
+#define HDA_ADSP_REG_HIPCI_BUSY		(0x1 << 31)
+
+/* HIPCIE */
+#define HDA_ADSP_REG_HIPCIE_DONE	(0x1 << 30)
+
+/* HIPCCTL */
+#define HDA_ADSP_REG_HIPCCTL_DONE	(0x1 << 1)
+
+/* HIPCT */
+#define HDA_ADSP_REG_HIPCT_BUSY		(0x1 << 31)
+
+/* Intel® HD Audio Code Loader DMA Registers */
+#define HDA_ADSP_LOADER_BASE	0x80
+
+/* Intel® HD Audio L2 local SRAM Window 1 */
+#define HDA_ADSP_SRAM1_BASE		0xA000
+
+/* Intel® HD Audio L2 local SRAM Window 2 */
+#define HDA_ADSP_SRAM2_BASE		0xC000
+
+/* Intel® HD Audio L2 local SRAM Window 3 */
+#define HDA_ADSP_SRAM3_BASE		0xE000
+
+#define HDA_ADSP_MMIO_LEN		0x10000
+
+#define WINDOW0_STAT_SIZE		0x800
+
+#define WINDOW0_UP_SIZE			0x800
+
+#define WINDOW1_SIZE			0x1000
+
+#define FW_STATUS_MASK			0xf
+
+#define FW_ROM_INIT			0x0
+#define FW_ROM_INIT_DONE		0x1
+#define FW_ROM_MEM_PWR_DONE		0x2
+#define FW_ROM_MANIFEST_LOADED		0X3
+#define FW_ROM_MANIFEST_VERIFIED	0x4
+#define FW_ROM_FEAT_MASK_VERIFIED	0x5
+#define FW_ROM_BASEFW_FOUND		0x6
+#define FW_ROM_BASEFW_BA_VALID		0x7
+#define FW_ROM_BASEFW_TOTAL_PWR		0x8
+#define FW_ROM_BASEFW_TEXT_LOADED	0x9
+#define FW_ROM_BASEFW_TEXT_HASHED	0xa
+#define FW_ROM_BASEFW_RODA_LOADED	0xb
+#define FW_ROM_BASEFW_LOAD_HASHED	0xc
+#define FW_ROM_BASEFW_HASH_VER		0xd
+#define FW_ROM_BASEFW_START_FOUND	0xe
+#define FW_ROM_BASEFW_ENTERED		0xf
+
+#define FW_CL_STREAM_NUMER	0xf
+
+#define DMA_ADDRESS_128_BITS_ALIGNMENT 7
+#define BDL_ALIGN(x)	(x >> DMA_ADDRESS_128_BITS_ALIGNMENT)
+
+#define ADSPIC_IPC                      1
+#define ADSPIS_IPC			1
+
+/* Code Loader -
+ * Stream Registers */
+#define HDA_ADSP_REG_CL_SD_CTL			(HDA_ADSP_LOADER_BASE + 0x00)
+#define HDA_ADSP_REG_CL_SD_STS			(HDA_ADSP_LOADER_BASE + 0x03)
+#define HDA_ADSP_REG_CL_SD_LPIB			(HDA_ADSP_LOADER_BASE + 0x04)
+#define HDA_ADSP_REG_CL_SD_CBL			(HDA_ADSP_LOADER_BASE + 0x08)
+#define HDA_ADSP_REG_CL_SD_LVI			(HDA_ADSP_LOADER_BASE + 0x0c)
+#define HDA_ADSP_REG_CL_SD_FIFOW		(HDA_ADSP_LOADER_BASE + 0x0e)
+#define HDA_ADSP_REG_CL_SD_FIFOSIZE		(HDA_ADSP_LOADER_BASE + 0x10)
+#define HDA_ADSP_REG_CL_SD_FORMAT		(HDA_ADSP_LOADER_BASE + 0x12)
+#define HDA_ADSP_REG_CL_SD_FIFOL		(HDA_ADSP_LOADER_BASE + 0x14)
+#define HDA_ADSP_REG_CL_SD_BDLPL		(HDA_ADSP_LOADER_BASE + 0x18)
+#define HDA_ADSP_REG_CL_SD_BDLPU		(HDA_ADSP_LOADER_BASE + 0x1c)
+
+/* Code Loader -
+ * Software Position Based FIFO Capability Registers */
+#define HDA_ADSP_REG_CL_SPBFIFO				(HDA_ADSP_LOADER_BASE + 0x20)
+#define HDA_ADSP_REG_CL_SPBFIFO_SPBFCH		(HDA_ADSP_REG_CL_SPBFIFO + 0x0)
+#define HDA_ADSP_REG_CL_SPBFIFO_SPBFCCTL	(HDA_ADSP_REG_CL_SPBFIFO + 0x4)
+#define HDA_ADSP_REG_CL_SPBFIFO_SPIB		(HDA_ADSP_REG_CL_SPBFIFO + 0x8)
+#define HDA_ADSP_REG_CL_SPBFIFO_MAXFIFOS	(HDA_ADSP_REG_CL_SPBFIFO + 0xc)
+
+
+/* Code Loader -
+ * Stream Descriptor x Control */
+/* Stream Reset */
+#define CL_SD_CTL_SRST_SHIFT	0
+#define CL_SD_CTL_SRST_MASK		(1 << CL_SD_CTL_SRST_SHIFT)
+#define CL_SD_CTL_SRST(x) \
+	((x << CL_SD_CTL_SRST_SHIFT) & CL_SD_CTL_SRST_MASK)
+
+/* Stream Run */
+#define CL_SD_CTL_RUN_SHIFT		1
+#define CL_SD_CTL_RUN_MASK		(1 << CL_SD_CTL_RUN_SHIFT)
+#define CL_SD_CTL_RUN(x) \
+	((x << CL_SD_CTL_RUN_SHIFT) & CL_SD_CTL_RUN_MASK)
+
+/* Interrupt On Completion Enable */
+#define CL_SD_CTL_IOCE_SHIFT	2
+#define CL_SD_CTL_IOCE_MASK		(1 << CL_SD_CTL_IOCE_SHIFT)
+#define CL_SD_CTL_IOCE(x) \
+	((x << CL_SD_CTL_IOCE_SHIFT) & CL_SD_CTL_IOCE_MASK)
+
+/* FIFO Error Interrupt Enable */
+#define CL_SD_CTL_FEIE_SHIFT	3
+#define CL_SD_CTL_FEIE_MASK		(1 << CL_SD_CTL_FEIE_SHIFT)
+#define CL_SD_CTL_FEIE(x) \
+	((x << CL_SD_CTL_FEIE_SHIFT) & CL_SD_CTL_FEIE_MASK)
+
+/* Descriptor Error Interrupt Enable */
+#define CL_SD_CTL_DEIE_SHIFT	4
+#define CL_SD_CTL_DEIE_MASK		(1 << CL_SD_CTL_DEIE_SHIFT)
+#define CL_SD_CTL_DEIE(x) \
+	((x << CL_SD_CTL_DEIE_SHIFT) & CL_SD_CTL_DEIE_MASK)
+
+/* FIFO Limit Change */
+#define CL_SD_CTL_FIFOLC_SHIFT	5
+#define CL_SD_CTL_FIFOLC_MASK	(1 << CL_SD_CTL_FIFOLC_SHIFT)
+#define CL_SD_CTL_FIFOLC(x) \
+	((x << CL_SD_CTL_FIFOLC_SHIFT) & CL_SD_CTL_FIFOLC_MASK)
+
+/* Stripe Control */
+#define CL_SD_CTL_STRIPE_SHIFT	16
+#define CL_SD_CTL_STRIPE_MASK	(0x3 << CL_SD_CTL_STRIPE_SHIFT)
+#define CL_SD_CTL_STRIPE(x) \
+	((x << CL_SD_CTL_STRIPE_SHIFT) & CL_SD_CTL_STRIPE_MASK)
+
+/* Traffic Priority */
+#define CL_SD_CTL_TP_SHIFT		18
+#define CL_SD_CTL_TP_MASK		(1 << CL_SD_CTL_TP_SHIFT)
+#define CL_SD_CTL_TP(x) \
+	((x << CL_SD_CTL_TP_SHIFT) & CL_SD_CTL_TP_MASK)
+
+/* Bidirectional Direction Control */
+#define CL_SD_CTL_DIR_SHIFT		19
+#define CL_SD_CTL_DIR_MASK		(1 << CL_SD_CTL_DIR_SHIFT)
+#define CL_SD_CTL_DIR(x) \
+	((x << CL_SD_CTL_DIR_SHIFT) & CL_SD_CTL_DIR_MASK)
+
+/* Stream Number */
+#define CL_SD_CTL_STRM_SHIFT	20
+#define CL_SD_CTL_STRM_MASK		(0xf << CL_SD_CTL_STRM_SHIFT)
+#define CL_SD_CTL_STRM(x) \
+	((x << CL_SD_CTL_STRM_SHIFT) & CL_SD_CTL_STRM_MASK)
+
+
+/* Code Loader -
+ * Stream Descriptor x Status */
+/* Buffer Completion Interrupt Status */
+#define CL_SD_STS_BCIS(x)		CL_SD_CTL_IOCE(x)
+
+/* FIFO Error */
+#define CL_SD_STS_FIFOE(x)		CL_SD_CTL_FEIE(x)
+
+/* Descriptor Error */
+#define CL_SD_STS_DESE(x)		CL_SD_CTL_DEIE(x)
+
+/* FIFO Ready */
+#define CL_SD_STS_FIFORDY(x)	CL_SD_CTL_FIFOLC(x)
+
+
+/* Code Loader -
+ * Stream Descriptor x Last Valid Index */
+#define CL_SD_LVI_SHIFT		0
+#define CL_SD_LVI_MASK		(0xff << CL_SD_LVI_SHIFT)
+#define CL_SD_LVI(x)		((x << CL_SD_LVI_SHIFT) & CL_SD_LVI_MASK)
+
+
+/* Code Loader -
+ * Stream Descriptor x FIFO Eviction Watermark */
+#define CL_SD_FIFOW_SHIFT	0
+#define CL_SD_FIFOW_MASK	(0x7 << CL_SD_FIFOW_SHIFT)
+#define CL_SD_FIFOW(x)		((x << CL_SD_FIFOW_SHIFT) & CL_SD_FIFOW_MASK)
+
+
+/* Code Loader -
+ * Stream Descriptor x Buffer Descriptor List Pointer Lower Base Address */
+/* Protect */
+#define CL_SD_BDLPLBA_PROT_SHIFT	0
+#define CL_SD_BDLPLBA_PROT_MASK		(1 << CL_SD_BDLPLBA_PROT_SHIFT)
+#define CL_SD_BDLPLBA_PROT(x) \
+	((x << CL_SD_BDLPLBA_PROT_SHIFT) & CL_SD_BDLPLBA_PROT_MASK)
+
+/* Buffer Descriptor List Lower Base Address */
+#define CL_SD_BDLPLBA_SHIFT	7
+#define CL_SD_BDLPLBA_MASK	(0x1ffffff << CL_SD_BDLPLBA_SHIFT)
+#define CL_SD_BDLPLBA(x) \
+	((BDL_ALIGN(lower_32_bits(x)) << CL_SD_BDLPLBA_SHIFT) & CL_SD_BDLPLBA_MASK)
+
+/* Buffer Descriptor List Upper Base Address */
+#define CL_SD_BDLPUBA_SHIFT	0
+#define CL_SD_BDLPUBA_MASK	(0xffffffff << CL_SD_BDLPUBA_SHIFT)
+#define CL_SD_BDLPUBA(x) \
+	((upper_32_bits(x) << CL_SD_BDLPUBA_SHIFT) & CL_SD_BDLPUBA_MASK)
+
+/* Code Loader - Software Position Based FIFO
+ * Capability Registers x Software Position Based FIFO Header */
+
+/* Next Capability Pointer */
+#define CL_SPBFIFO_SPBFCH_PTR_SHIFT	0
+#define CL_SPBFIFO_SPBFCH_PTR_MASK	(0xff << CL_SPBFIFO_SPBFCH_PTR_SHIFT)
+#define CL_SPBFIFO_SPBFCH_PTR(x) \
+	((x << CL_SPBFIFO_SPBFCH_PTR_SHIFT) & CL_SPBFIFO_SPBFCH_PTR_MASK)
+
+/* Capability Identifier */
+#define CL_SPBFIFO_SPBFCH_ID_SHIFT	16
+#define CL_SPBFIFO_SPBFCH_ID_MASK	(0xfff << CL_SPBFIFO_SPBFCH_ID_SHIFT)
+#define CL_SPBFIFO_SPBFCH_ID(x) \
+	((x << CL_SPBFIFO_SPBFCH_ID_SHIFT) & CL_SPBFIFO_SPBFCH_ID_MASK)
+
+/* Capability Version */
+#define CL_SPBFIFO_SPBFCH_VER_SHIFT	28
+#define CL_SPBFIFO_SPBFCH_VER_MASK	(0xf << CL_SPBFIFO_SPBFCH_VER_SHIFT)
+#define CL_SPBFIFO_SPBFCH_VER(x) \
+	((x << CL_SPBFIFO_SPBFCH_VER_SHIFT) & CL_SPBFIFO_SPBFCH_VER_MASK)
+
+
+/* Code Loader -
+   Software Position Based FIFO Control */
+/* Software Position in Buffer Enable */
+#define CL_SPBFIFO_SPBFCCTL_SPIBE_SHIFT	0
+#define CL_SPBFIFO_SPBFCCTL_SPIBE_MASK	(1 << CL_SPBFIFO_SPBFCCTL_SPIBE_SHIFT)
+#define CL_SPBFIFO_SPBFCCTL_SPIBE(x) \
+	((x << CL_SPBFIFO_SPBFCCTL_SPIBE_SHIFT) & CL_SPBFIFO_SPBFCCTL_SPIBE_MASK)
+
+
+/* ADSPCS - Audio DSP Control & Status */
+#define DSP_CORES 2
+#define DSP_CORES_MASK ((1 << DSP_CORES) - 1)
+
+/* Core Reset - asserted high */
+#define ADSPCS_CRST_SHIFT	0
+#define ADSPCS_CRST_MASK	(DSP_CORES_MASK << ADSPCS_CRST_SHIFT)
+#define ADSPCS_CRST(x)		((x << ADSPCS_CRST_SHIFT) & ADSPCS_CRST_MASK)
+
+/* Core run/stall - when set to '1' core is stalled */
+#define ADSPCS_CSTALL_SHIFT	8
+#define ADSPCS_CSTALL_MASK	(DSP_CORES_MASK << ADSPCS_CSTALL_SHIFT)
+#define ADSPCS_CSTALL(x)	((x << ADSPCS_CSTALL_SHIFT) & ADSPCS_CSTALL_MASK)
+
+/* Set Power Active - when set to '1' turn cores on */
+#define ADSPCS_SPA_SHIFT	16
+#define ADSPCS_SPA_MASK		(DSP_CORES_MASK << ADSPCS_SPA_SHIFT)
+#define ADSPCS_SPA(x)		((x << ADSPCS_SPA_SHIFT) & ADSPCS_SPA_MASK)
+
+/* Current Power Active - power status of cores, set by hardware */
+#define ADSPCS_CPA_SHIFT	24
+#define ADSPCS_CPA_MASK		(DSP_CORES_MASK << ADSPCS_CPA_SHIFT)
+#define ADSPCS_CPA(x)		((x << ADSPCS_CPA_SHIFT) & ADSPCS_CPA_MASK)
+
+
+/* Broxton specificdefinitions */
+
+#define HDA_ADSP_REG_ADSPCS_IMR_CACHED_TLB_START 0x100
+#define HDA_ADSP_REG_ADSPCS_IMR_UNCACHED_TLB_START 0x200
+
+/* sst definition */
+struct ipc;
+struct sst_dsp_ctx;
+
+struct sst_window {
+	void __iomem *w0stat;
+	void __iomem *w0up;
+	void __iomem *w1;
+	size_t w0stat_size;
+	size_t w0up_size;
+	size_t w1_size;
+};
+
+struct sst_dsp_loader_ops {
+	int (*init) (struct device *dev);
+	int (*prepare) (struct device *dev, unsigned int format,
+				unsigned int byte_size,
+				struct snd_dma_buffer *bufp);
+	void (*trigger) (struct device *dev, bool start);
+
+	void (*cleanup) (struct device *dev,
+				 struct snd_dma_buffer *dmab);
+	int (*alloc_dma_buf) (struct device *dev,
+		struct snd_dma_buffer *dmab, u32 size);
+	int (*free_dma_buf) (struct device *dev,
+		struct snd_dma_buffer *dmab);
+};
+
+struct sst_ops {
+	int (*load_fw)(struct sst_dsp_ctx  *ctx);
+	/* FW module parser/loader */
+	int (*parse_fw)(struct sst_dsp_ctx *ctx);
+};
+
+struct sst_dsp_ctx {
+	struct device *dev;
+	struct ipc *ipc;
+	void __iomem *mmio_base;
+	struct sst_window window;
+	spinlock_t reg_lock;
+	int irq;
+	struct sst_dsp_loader_ops  dsp_ops;
+	struct sst_ops ops;
+	struct snd_dma_buffer dsp_fw_buf;
+};
+
+int sst_load_skl_base_firmware(struct sst_dsp_ctx *ctx);
+int sst_load_bxt_base_firmware(struct sst_dsp_ctx *ctx);
+
+void sst_updatel_bits(struct sst_dsp_ctx *ctx, u32 offset, u32 mask, u32 value);
+
+void sst_mailbox_write(struct sst_dsp_ctx *ctx, void *message, size_t bytes);
+void sst_mailbox_read(struct sst_dsp_ctx *ctx, void *message, size_t bytes);
+
+int sst_dsp_init(struct sst_dsp_ctx *ctx);
+int sst_skl_init(struct device *dev, void __iomem *mmio_base, int irq,
+		struct sst_dsp_loader_ops dsp_ops, struct sst_dsp_ctx **dsp);
+int sst_bxt_init(struct device *dev, void __iomem *mmio_base, int irq,
+		struct sst_dsp_loader_ops dsp_ops, struct sst_dsp_ctx **dsp);
+int sst_dsp_free(struct sst_dsp_ctx *dsp);
+u8 sst_readb_traced(
+	struct sst_dsp_ctx *ctx,
+	u32 offset);
+
+u16 sst_readw_traced(
+	struct sst_dsp_ctx *ctx,
+	u32 offset);
+
+u32 sst_readl_traced(
+	struct sst_dsp_ctx *ctx,
+	u32 offset);
+
+void sst_writeb_traced(
+	struct sst_dsp_ctx *ctx,
+	u32 offset,
+	u8 val);
+
+void sst_writew_traced(
+	struct sst_dsp_ctx *ctx,
+	u32 offset, u16 val);
+
+void sst_writel_traced(
+	struct sst_dsp_ctx *ctx,
+	u32 offset,
+	u32 val);
+
+int sst_boot_dsp(struct sst_dsp_ctx *ctx);
+
+int sst_register_poll(struct sst_dsp_ctx  *ctx, u32 offset, u32 mask,
+			 u32 expected_value, u32 timeout, char *operation);
+int sst_enable_dsp_core(struct sst_dsp_ctx  *ctx);
+int sst_disable_dsp_core(struct sst_dsp_ctx  *ctx);
+
+#endif /*__HDA_SST_DSP_H__*/
---
+++ sound/soc/hda/intel/soc-hda-sst-dsp.c
@@ -0,0 +1,450 @@
+/*
+ *  soc_hda_sst-dsp.c - HDA DSP library generic function
+ *
+ *  Copyright (C) 2014 Intel Corp
+ *  Author:Rafal Redzimski <rafal.f.redzimski@intel.com>
+ *		Jeeja KP <jeeja.kp@intel.com>
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+#include <linux/io.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/spinlock_types.h>
+#include <linux/mutex.h>
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/memalloc.h>
+#include <sound/soc-hda-sst-dsp.h>
+#include <sound/soc-hda-sst-ipc.h>
+
+#define DSP_CORE_POWER_UP_TIMEOUT		50
+#define DSP_CORE_POWER_DOWN_TIMEOUT		50
+#define DSP_CORE_SET_RESET_STATE_TIMEOUT	50
+#define DSP_CORE_UNSET_RESET_STATE_TIMEOUT	50
+
+#ifdef DEBUG
+u8 sst_readb_traced(struct sst_dsp_ctx *dsp, u32 offset)
+{
+	u8 val;
+
+	val = readb((dsp)->mmio_base + offset);
+	dev_dbg(dsp->dev, "offset=%x val=%x\n", offset, val);
+
+	return val;
+}
+
+u16 sst_readw_traced(struct sst_dsp_ctx *dsp, u32 offset)
+{
+	u16 val;
+
+	val = readw((dsp)->mmio_base + offset);
+	dev_dbg(dsp->dev, "offset=%x val=%x\n", offset, val);
+
+	return val;
+}
+
+u32 sst_readl_traced(struct sst_dsp_ctx *dsp, u32 offset)
+{
+	u32 val;
+
+	val = readl((dsp)->mmio_base + offset);
+	dev_dbg(dsp->dev, "offset=%x val=%x\n", offset, val);
+
+	return val;
+}
+
+void sst_writeb_traced(struct sst_dsp_ctx *dsp, u32 offset, u8 val)
+{
+	writeb(val, (dsp)->mmio_base + offset);
+	dev_dbg(dsp->dev, "offset=%x val=%x\n", offset, val);
+}
+
+void sst_writew_traced(struct sst_dsp_ctx *dsp, u32 offset, u16 val)
+{
+	dev_dbg(dsp->dev, "offset=%x val=%x\n", offset, val);
+	writew(val, (dsp)->mmio_base + offset);
+}
+
+void sst_writel_traced(struct sst_dsp_ctx *dsp, u32 offset, u32 val)
+{
+	dev_dbg(dsp->dev, "offset=%x val=%x\n", offset, val);
+	writel(val, (dsp)->mmio_base + offset);
+}
+#endif
+
+/* generic functions */
+static irqreturn_t sst_interrupt(int irq, void *dev_id);
+static int sst_acquire_irq(struct sst_dsp_ctx *dsp);
+
+void sst_updatel_bits(
+	struct sst_dsp_ctx *ctx,
+	u32 offset, u32 mask, u32 value)
+{
+	u32 val;
+
+	val = (readl((ctx)->mmio_base + offset) & ~mask) | (value & mask);
+
+	writel(val, (ctx)->mmio_base + offset);
+
+	dev_dbg(ctx->dev, "offset=%x val=%x\n", offset, val);
+}
+
+static void sst_updatel_bits_locked(
+	struct sst_dsp_ctx  *ctx,
+	u32 offset, u32 mask, u32 value)
+{
+	unsigned long flags;
+	spin_lock_irqsave(&ctx->reg_lock, flags);
+	sst_updatel_bits(ctx, offset, mask, value);
+	spin_unlock_irqrestore(&ctx->reg_lock, flags);
+}
+
+static inline void _sst_memcpy_toio_32(volatile u32 __iomem *dest,
+	u32 *src, size_t bytes)
+{
+	int i, words = bytes >> 2;
+
+	for (i = 0; i < words; i++)
+		writel(src[i], dest + i);
+}
+
+static inline void _sst_memcpy_fromio_32(u32 *dest,
+	const volatile __iomem u32 *src, size_t bytes)
+{
+	int i, words = bytes >> 2;
+
+	for (i = 0; i < words; i++)
+		dest[i] = readl(src + i);
+}
+
+void sst_mailbox_write(struct sst_dsp_ctx  *ctx, void *msg, size_t bytes)
+{
+	_sst_memcpy_toio_32(ctx->window.w1, msg, bytes);
+}
+
+void sst_mailbox_read(struct sst_dsp_ctx  *ctx, void *msg, size_t bytes)
+{
+	_sst_memcpy_fromio_32(msg, ctx->window.w0up, bytes);
+}
+
+int sst_register_poll(struct sst_dsp_ctx  *ctx, u32 offset, u32 mask,
+			 u32 expected_value, u32 timeout, char *operation)
+{
+	int time = 0;
+	int ret = 0;
+	u32 reg;
+
+
+	/* check if set state successful */
+	for (time = 0; time < timeout; time++) {
+		if ((sst_readl_alt(ctx, offset) & mask) == expected_value)
+			break;
+
+		mdelay(1);
+	}
+	reg = sst_readl_alt(ctx, offset);
+	dev_dbg(ctx->dev, "FW Status = reg:0x%x time=%d ret=%d\n", reg, time, (time < timeout));
+	ret = time < timeout ? 0 : -ETIME;
+
+	return ret;
+}
+
+static int sst_dsp_core_set_reset_state(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	/* update bits */
+	sst_updatel_locked(ctx, ADSPCS, CRST_MASK,
+				ADSPCS_CRST(DSP_CORES_MASK));
+	/* poll with timeout to check if operation successful */
+	ret = sst_register_poll(ctx,
+			HDA_ADSP_REG_ADSPCS,
+			ADSPCS_CRST_MASK,
+			ADSPCS_CRST(DSP_CORES_MASK),
+			DSP_CORE_SET_RESET_STATE_TIMEOUT,
+			"Set reset");
+
+	snd_BUG_ON((sst_readl(ctx, ADSPCS) & ADSPCS_CRST(DSP_CORES_MASK)) !=
+			ADSPCS_CRST(DSP_CORES_MASK));
+
+	return ret;
+}
+
+static int sst_dsp_core_unset_reset_state(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	/* update bits */
+	sst_updatel_locked(ctx, ADSPCS, CRST_MASK, 0);
+
+	/* poll with timeout to check if operation successful */
+	ret = sst_register_poll(ctx,
+			HDA_ADSP_REG_ADSPCS,
+			ADSPCS_CRST_MASK,
+			0,
+			DSP_CORE_UNSET_RESET_STATE_TIMEOUT,
+			"Unset reset");
+
+	snd_BUG_ON((sst_readl(ctx, ADSPCS) & ADSPCS_CRST(DSP_CORES_MASK)) !=
+			0);
+
+	return ret;
+}
+
+static int sst_dsp_core_power_up(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	dev_dbg(ctx->dev, "In %s\n", __func__);
+	/* update bits */
+	sst_updatel_locked(ctx,
+			ADSPCS, SPA_MASK, ADSPCS_SPA(DSP_CORES_MASK));
+
+	/* poll with timeout to check if operation successful */
+	ret = sst_register_poll(ctx,
+			HDA_ADSP_REG_ADSPCS,
+			ADSPCS_CPA_MASK,
+			ADSPCS_CPA(DSP_CORES_MASK),
+			DSP_CORE_POWER_UP_TIMEOUT,
+			"Power up");
+
+	snd_BUG_ON((sst_readl(ctx, ADSPCS) & ADSPCS_CPA(DSP_CORES_MASK)) !=
+			ADSPCS_CPA(DSP_CORES_MASK));
+
+	return ret;
+}
+
+static int sst_dsp_core_power_down(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	/* update bits */
+	sst_updatel_locked(ctx, ADSPCS, SPA_MASK, 0);
+
+	/* poll with timeout to check if operation successful */
+	ret = sst_register_poll(ctx,
+			HDA_ADSP_REG_ADSPCS,
+			ADSPCS_SPA_MASK,
+			0,
+			DSP_CORE_POWER_DOWN_TIMEOUT,
+			"Power down");
+
+	return ret;
+}
+
+static bool sst_is_dsp_core_enable(struct sst_dsp_ctx  *ctx)
+{
+	int val = 0;
+	bool is_enable;
+
+	val = sst_readl(ctx, ADSPCS);
+
+	is_enable = ((val & ADSPCS_CPA(DSP_CORES_MASK)) &&
+			(val & ADSPCS_SPA(DSP_CORES_MASK)) &&
+			!(val & ADSPCS_CRST(DSP_CORES_MASK)) &&
+			!(val & ADSPCS_CSTALL(DSP_CORES_MASK)));
+
+	dev_dbg(ctx->dev, "DSP core is enabled=%d\n", is_enable);
+	return is_enable;
+}
+
+
+int sst_enable_dsp_core(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	dev_dbg(ctx->dev, "In %s\n", __func__);
+
+	/* power up */
+	ret = sst_dsp_core_power_up(ctx);
+	if (ret < 0) {
+		dev_dbg(ctx->dev, "dsp core power up failed\n");
+		return ret;
+	}
+
+	/* unset reset state */
+	ret = sst_dsp_core_unset_reset_state(ctx);
+	if (ret < 0) {
+		dev_dbg(ctx->dev, "dsp core reset failed\n");
+		return ret;
+	}
+
+	/* run core */
+	sst_writel(ctx, ADSPCS, sst_readl(ctx, ADSPCS) &
+			~ADSPCS_CSTALL(DSP_CORES_MASK));
+
+	snd_BUG_ON(!sst_is_dsp_core_enable(ctx));
+
+	return ret;
+}
+
+int sst_disable_dsp_core(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	dev_dbg(ctx->dev, "In %s\n", __func__);
+
+	/* stall core */
+	sst_writel(ctx, ADSPCS, sst_readl(ctx, ADSPCS) |
+			ADSPCS_CSTALL(DSP_CORES_MASK));
+
+	/* set reset state */
+	ret = sst_dsp_core_set_reset_state(ctx);
+	if (ret < 0) {
+		dev_err(ctx->dev, "dsp core reset failed\n");
+		return ret;
+	}
+
+	/* power down core*/
+	ret = sst_dsp_core_power_down(ctx);
+	if (ret < 0) {
+		dev_err(ctx->dev, "dsp core power down failed\n");
+		return ret;
+	}
+
+	snd_BUG_ON(sst_is_dsp_core_enable(ctx));
+
+	return ret;
+}
+
+static int sst_reset_dsp_core(struct sst_dsp_ctx  *ctx)
+{
+	int ret = 0;
+
+	dev_dbg(ctx->dev, "In %s\n", __func__);
+
+	/* stall core */
+	sst_writel(ctx, ADSPCS, sst_readl(ctx, ADSPCS) &
+			ADSPCS_CSTALL(DSP_CORES_MASK));
+
+	/* set reset state */
+	ret = sst_dsp_core_set_reset_state(ctx);
+	if (ret < 0) {
+		dev_err(ctx->dev, "dsp reset failed\n");
+		return ret;
+	}
+
+	/* unset reset state */
+	ret = sst_dsp_core_unset_reset_state(ctx);
+	if (ret < 0) {
+		dev_dbg(ctx->dev, "dsp unset reset failes\n");
+		return ret;
+	}
+
+	/* run core */
+	sst_writel(ctx, ADSPCS, sst_readl(ctx, ADSPCS) &
+			~ADSPCS_CSTALL(DSP_CORES_MASK));
+
+	snd_BUG_ON(sst_is_dsp_core_enable(ctx));
+
+	return ret;
+}
+
+int sst_boot_dsp(struct sst_dsp_ctx *ctx)
+{
+	int ret = 0;
+
+	dev_dbg(ctx->dev, "In %s\n", __func__);
+
+	if (sst_is_dsp_core_enable(ctx)) {
+		/* Dsp core powered up - simply reset core */
+		dev_dbg(ctx->dev, "dsp core is already enabled, so reset the dap core\n");
+		ret = sst_reset_dsp_core(ctx);
+	} else {
+		/*disable and enable to make sure DSP is invalid state */
+		ret = sst_disable_dsp_core(ctx);
+
+		if (ret < 0) {
+			dev_err(ctx->dev, "dsp disable core failes\n");
+			return ret;
+		}
+		ret = sst_enable_dsp_core(ctx);
+	}
+
+	return ret;
+}
+
+int sst_dsp_init(struct sst_dsp_ctx *ctx)
+{
+	int ret = 0;
+
+	/* initialize IPC */
+	ctx->ipc = ipc_init(ctx->dev, ctx);
+	if (ctx->ipc == NULL)
+		ret = -ENODEV;
+
+	/* Now let's request the IRQ */
+	sst_acquire_irq(ctx);
+
+	return ret;
+}
+
+int sst_dsp_free(struct sst_dsp_ctx *dsp)
+{
+	int ret = 0;
+
+	ipc_int_disable(dsp);
+
+	free_irq(dsp->irq, dsp);
+
+	kfree(dsp);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(sst_dsp_free);
+
+/*
+ * interrupt handler
+ */
+static irqreturn_t sst_interrupt(int irq, void *dev_id)
+{
+	struct sst_dsp_ctx *ctx = (struct sst_dsp_ctx *) dev_id;
+	u32 val;
+	irqreturn_t result;
+	spin_lock(&ctx->reg_lock);
+
+	val = sst_readl(ctx, ADSPIS);
+
+	if (val & ADSPIS_IPC) {
+		ipc_int_disable(ctx);
+		result = ipc_irq_handler(ctx);
+		ipc_int_enable(ctx);
+		if (result == IRQ_HANDLED) {
+			spin_unlock(&ctx->reg_lock);
+			return IRQ_HANDLED;
+		}
+	}
+
+	spin_unlock(&ctx->reg_lock);
+	return IRQ_NONE;
+}
+
+static int sst_acquire_irq(struct sst_dsp_ctx *ctx)
+{
+	if (request_irq(ctx->irq, sst_interrupt,
+			IRQF_SHARED,
+			KBUILD_MODNAME, ctx)) {
+		dev_err(ctx->dev, "unable to grab IRQ %d, disabling device\n", ctx->irq);
+		return -1;
+	}
+	return 0;
+}
+
+MODULE_DESCRIPTION("HDA SST/IPC Library");
+MODULE_LICENSE("GPL v2");
