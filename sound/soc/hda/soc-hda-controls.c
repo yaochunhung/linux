4a941aaea5bca7384d6a891a3dc7bf23882082ad
Mythri P K <mythri.p.k@intel.com>
2014-11-06 22:00:03 +0530

    Audio: hda: update the mask bit in sst_mix_put
    The mask is always 1 and hence  update.mask should be shifted
    by the appropriate register bit position in the register.
    
    Change-Id: Id49dfcb7d6679a1898f6a6a50abf1f0b17e0f819
    Signed-off-by: Mythri P K <mythri.p.k@intel.com>
    Reviewed-on: https://android.intel.com/298164
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Shah, Hardik T <hardik.t.shah@intel.com>
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>

--- sound/soc/hda/soc-hda-controls.c
+++ sound/soc/hda/soc-hda-controls.c
@@ -154,7 +154,7 @@ int hda_sst_mix_put(struct snd_kcontrol *kcontrol,
 	dapm_kcontrol_set_value(kcontrol,  val);
 	update.kcontrol = kcontrol;
 	update.reg = mc->reg;
-	update.mask = mask;
+	update.mask = mask << mc->shift;
 	update.val = val;
 
 	snd_soc_dapm_mixer_update_power(dapm, kcontrol, connect, &update);
