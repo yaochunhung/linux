d354f9ef4358ced45bfbb443d2bbdaac7a59c0a8
Deepak S <deepak.s@intel.com>
2017-01-13 15:40:48 -0800

    drm/i915/vlv: Add valleyview RPS debugfs disabling
    This patch exposes a new debugfs interface 'i915_rps_disable'
    Following 2 values shall be echoed into this file.
    '0' - RPS explicitly enabled .
    '1' - RPS explicitly disabled.
    
    This interface provides capabilty to enable/disable Turbo feature
    at runtime, which is needed for its validation.
    
    Tracked-On:
    Issue: VIZ-3403
    Change-Id: Idff952a26c45fd42f1e940a030f9788a073c1bc8
    Signed-off-by: Deepak S <deepak.s@intel.com>
    Signed-off-by: Jeff McGee <jeff.mcgee@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/i915_debugfs.c
+++ drivers/gpu/drm/i915/i915_debugfs.c
@@ -4394,7 +4394,7 @@ static int i915_rps_disable_boost_get(void *data, u64 *val)
 	struct drm_device *dev = data;
 	struct drm_i915_private *dev_priv = dev->dev_private;
 
-	if (INTEL_INFO(dev)->gen < 6)
+	if (INTEL_INFO(dev_priv)->gen < 6)
 		return -ENODEV;
 
 	*val = dev_priv->rps.debugfs_disable_boost;
@@ -4424,6 +4424,58 @@ static int i915_rps_disable_boost_set(void *data, u64 val)
 
 DEFINE_SIMPLE_ATTRIBUTE(i915_rps_disable_boost_fops,
 		i915_rps_disable_boost_get, i915_rps_disable_boost_set,
+			"%llu\n");
+
+static int i915_rps_disable_get(void *data, u64 *val)
+{
+	struct drm_device *dev = data;
+	struct drm_i915_private *dev_priv = dev->dev_private;
+
+	if (INTEL_GEN(dev_priv) < 6)
+		return -ENODEV;
+
+	*val = dev_priv->rps.rps_disable;
+
+	return 0;
+}
+
+static int i915_rps_disable_set(void *data, u64 val)
+{
+	struct drm_device *dev = data;
+	struct drm_i915_private *dev_priv = dev->dev_private;
+	int ret;
+
+	if (INTEL_GEN(dev_priv) < 6)
+		return -ENODEV;
+
+	DRM_DEBUG_DRIVER("Setting RPS disable %s\n",
+			 val ? "true" : "false");
+
+	intel_runtime_pm_get(dev_priv);
+	ret = mutex_lock_interruptible(&dev_priv->rps.hw_lock);
+	if (ret)
+		return ret;
+
+	dev_priv->rps.rps_disable = val;
+
+	if (val)
+		I915_WRITE(GEN6_RP_CONTROL, 0);
+	else
+		I915_WRITE(GEN6_RP_CONTROL, GEN6_RP_MEDIA_TURBO |
+				GEN6_RP_MEDIA_HW_NORMAL_MODE |
+				GEN6_RP_MEDIA_IS_GFX |
+				GEN6_RP_ENABLE |
+				GEN6_RP_UP_BUSY_AVG |
+				GEN6_RP_DOWN_IDLE_AVG);
+
+	mutex_unlock(&dev_priv->rps.hw_lock);
+	intel_runtime_pm_put(dev_priv);
+
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(i915_rps_disable_fops,
+			i915_rps_disable_get, i915_rps_disable_set,
 			"%llu\n");
 
 static int i915_rc6_disable_get(void *data, u64 *val)
@@ -4807,6 +4859,7 @@ static const struct i915_debugfs_files {
 	{"i915_max_freq", &i915_max_freq_fops},
 	{"i915_min_freq", &i915_min_freq_fops},
 	{"i915_rps_disable_boost", &i915_rps_disable_boost_fops},
+	{"i915_rps_disable", &i915_rps_disable_fops},
 	{"i915_rc6_disable", &i915_rc6_disable_fops},
 	{"i915_cache_sharing", &i915_cache_sharing_fops},
 	{"i915_ring_missed_irq", &i915_ring_missed_irq_fops},
