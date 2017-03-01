9282aa4298dc39f5a464c5587700de2bbe553e69
Chris Wilson <chris@chris-wilson.co.uk>
2017-03-01 10:47:45 -0800

    drm/i915: Include the kernel uptime in the error state
    As well as knowing when the error occurred, it is more interesting to me
    to know how long after booting the error occurred, and for good measure
    record the time since last hw initialisation.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161025121602.1457-1-chris@chris-wilson.co.uk
    (cherry picked from commit de867c20b94b6d52a0b10cb02f10a1f475abf7f5)
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -746,6 +746,8 @@ struct intel_display_error_state;
 struct drm_i915_error_state {
 	struct kref ref;
 	struct timeval time;
+	struct timeval boottime;
+	struct timeval uptime;
 
 	struct drm_i915_private *i915;
 
@@ -2098,6 +2100,8 @@ struct drm_i915_private {
 		 * off the idle_work.
 		 */
 		struct delayed_work idle_work;
+
+		ktime_t last_init_time;
 	} gt;
 
 	/* perform PHY state sanity checks? */
