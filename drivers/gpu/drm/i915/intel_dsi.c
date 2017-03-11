axxia/axxia_yocto_linux_4.1

drm/i915: Check for platform specific GPIO config
Panel GPIO control should be done based on platform. Add a check
to restrict BYT specific GPIO confirguration for only BYT. Avoid
the same for BXT.

Signed-off-by: Uma Shankar <uma.shankar@intel.com>
Signed-off-by: Bob Paauwe <bob.j.paauwe@intel.com>
Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

drivers/gpu/drm/i915/intel_dsi.c
@@ -1584,7 +1584,8 @@ void intel_dsi_init(struct drm_device *dev)
 	 * In case of BYT with CRC PMIC, we need to use GPIO for
 	 * Panel control.
 	 */
-	if (dev_priv->vbt.dsi.config->pwm_blc == PPS_BLC_PMIC) {
+	if (!IS_BROXTON(dev) &&
+		(dev_priv->vbt.dsi.config->pwm_blc == PPS_BLC_PMIC)) {
 		intel_dsi->gpio_panel =
 			gpiod_get(dev->dev, "panel", GPIOD_OUT_HIGH);
 

yaochunhung/linux

drivers/gpu/drm/i915/intel_dsi.c
@@ -1578,7 +1578,8 @@ void intel_dsi_init(struct drm_i915_private *dev_priv)
 	 * In case of BYT with CRC PMIC, we need to use GPIO for
 	 * Panel control.
 	 */
-	if (dev_priv->vbt.dsi.config->pwm_blc == PPS_BLC_PMIC) {
+	if (!IS_BROXTON(dev_priv) &&
+		(dev_priv->vbt.dsi.config->pwm_blc == PPS_BLC_PMIC)) {
 		intel_dsi->gpio_panel =
 			gpiod_get(dev->dev, "panel", GPIOD_OUT_HIGH);
 