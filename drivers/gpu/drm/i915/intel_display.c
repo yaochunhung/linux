axxia/axxia_yocto_linux_4.1

drm/i915/skl+: Enable gamma and CSC on bottom color. (v2)
To stay consisent with how we're programming all the other planes,
enable gamma and CSC on the bottom color.  Without this, we fail the
the kms_universal_plane functional tests because the black primary plane
is brighter (gamma corrected) than the disabled plane case.  If the bottom
color is also gamma/csc corrected, then the disiabled case will match the
black plane case.

v2: Instead of preserving the existing bottom color, set it to black
    as we don't yet have any way to set it to another color. (Matt)

testcase: igt/kms_universal_plane/universal-plane-pipe-[ABC]-functional
CC: Konduru, Chandra <chandra.konduru@intel.com>
cc: Kevin Strasser <kevin.strasser@linux.intel.com>
Signed-off-by: Bob Paauwe <bob.j.paauwe@intel.com>
Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

drivers/gpu/drm/i915/intel_display.c
@@ -4810,6 +4810,7 @@ static void haswell_crtc_enable(struct drm_crtc *crtc)
 	enum transcoder cpu_transcoder = intel_crtc->config->cpu_transcoder;
 	struct intel_crtc_state *pipe_config =
 		to_intel_crtc_state(crtc->state);
+	u32 bottom;
 
 	if (WARN_ON(intel_crtc->active))
 		return;
@@ -4917,6 +4918,11 @@ static void haswell_crtc_enable(struct drm_crtc *crtc)
 	if (IS_HASWELL(dev) && hsw_workaround_pipe != INVALID_PIPE) {
 		intel_wait_for_vblank(dev, hsw_workaround_pipe);
 		intel_wait_for_vblank(dev, hsw_workaround_pipe);
+	}
+
+	if (INTEL_INFO(dev)->gen >= 9) {
+		bottom = (PIPE_BOTTOM_CSC_ENABLE | PIPE_BOTTOM_GAMMA_ENABLE);
+		I915_WRITE(PIPE_BOTTOM_COLOR(pipe), bottom);
 	}
 }
 

yaochunhung/linux

drivers/gpu/drm/i915/intel_display.c
@@ -5379,6 +5379,7 @@ static void haswell_crtc_enable(struct intel_crtc_state *pipe_config,
 	enum transcoder cpu_transcoder = intel_crtc->config->cpu_transcoder;
 	struct intel_atomic_state *old_intel_state =
 		to_intel_atomic_state(old_state);
+	u32 bottom;
 
 	if (WARN_ON(intel_crtc->active))
 		return;
@@ -5480,6 +5481,11 @@ static void haswell_crtc_enable(struct intel_crtc_state *pipe_config,
 	if (IS_HASWELL(dev_priv) && hsw_workaround_pipe != INVALID_PIPE) {
 		intel_wait_for_vblank(dev_priv, hsw_workaround_pipe);
 		intel_wait_for_vblank(dev_priv, hsw_workaround_pipe);
+	}
+
+	if (INTEL_INFO(dev_priv)->gen >= 9) {
+		bottom = (PIPE_BOTTOM_CSC_ENABLE | PIPE_BOTTOM_GAMMA_ENABLE);
+		I915_WRITE(PIPE_BOTTOM_COLOR(pipe), bottom);
 	}
 }
 