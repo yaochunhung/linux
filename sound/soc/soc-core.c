9b134132f7eec5b7a706a006b9a17bb1af989ea9
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-05-08 03:22:11 +0000

    ASoC: soc-core: remove snd_soc_component_add_unlocked()
    There is no user to call snd_soc_component_add_unlocked() anymore.
    Let's merge snd_soc_component_add_unlocked() and
    snd_soc_component_add().
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 359c71eeacbdd93d7870035af9a830140798fc67)

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -3053,8 +3053,10 @@ EXPORT_SYMBOL_GPL(snd_soc_component_exit_regmap);
 
 #endif
 
-static void snd_soc_component_add_unlocked(struct snd_soc_component *component)
+static void snd_soc_component_add(struct snd_soc_component *component)
 {
+	mutex_lock(&client_mutex);
+
 	if (!component->driver->write && !component->driver->read) {
 		if (!component->regmap)
 			component->regmap = dev_get_regmap(component->dev, NULL);
@@ -3064,12 +3066,7 @@ static void snd_soc_component_add_unlocked(struct snd_soc_component *component)
 
 	list_add(&component->list, &component_list);
 	INIT_LIST_HEAD(&component->dobj_list);
-}
 
-static void snd_soc_component_add(struct snd_soc_component *component)
-{
-	mutex_lock(&client_mutex);
-	snd_soc_component_add_unlocked(component);
 	mutex_unlock(&client_mutex);
 }
 
