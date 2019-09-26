d5893fc929c3179d8a2fa3bf9bfa8991347da294
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-09-26 18:32:52 -0500

    soundwire: intel: call helper to reset Slave states on resume
    This helps make sure they are all UNATTACHED and reset the state
    machines.
    
    At the moment we perform a bus reset both for resume and pm_resume,
    this will be modified when clock-stop mode is supported
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1446,6 +1446,13 @@ static int intel_resume(struct device *dev)
 		return ret;
 	}
 
+	/*
+	 * make sure all Slaves are tagged as UNATTACHED and provide
+	 * reason for reinitialization
+	 */
+	sdw_clear_slave_status(&sdw->cdns.bus,
+			       SDW_UNATTACH_REQUEST_MASTER_RESET);
+
 	ret = sdw_cdns_enable_interrupt(cdns, true);
 	if (ret < 0) {
 		dev_err(dev, "cannot enable interrupts during resume\n");
@@ -1490,6 +1497,13 @@ static int intel_resume_runtime(struct device *dev)
 		dev_err(dev, "%s failed: %d", __func__, ret);
 		return ret;
 	}
+
+	/*
+	 * make sure all Slaves are tagged as UNATTACHED and provide
+	 * reason for reinitialization
+	 */
+	sdw_clear_slave_status(&sdw->cdns.bus,
+			       SDW_UNATTACH_REQUEST_MASTER_RESET);
 
 	ret = sdw_cdns_enable_interrupt(cdns, true);
 	if (ret < 0) {
