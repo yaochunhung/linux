83cd507653c3772c1cc57f9286bd7545407c4add
Keyon Jie <yang.jie@linux.intel.com>
2019-10-09 10:08:45 +0800

    ASoC: SOF: add a flag suspend_ignored for sof stream
    Some stream may be active at low power suspended state, e.g. d0i3
    compatible stream, add flag suspend_ignored to mark about this.
    
    Change-Id: I67eceb937d0200936dedeca3cfed22179ce33315
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- sound/soc/sof/sof-priv.h
+++ sound/soc/sof/sof-priv.h
@@ -307,6 +307,7 @@ struct snd_sof_pcm_stream {
 	struct snd_pcm_substream *substream;
 	struct work_struct period_elapsed_work;
 	bool d0i3_compatible; /* DSP can be in D0I3 when this pcm is opened */
+	bool suspend_ignored;
 };
 
 /* ALSA SOF PCM device */
