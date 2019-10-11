31c3f8dfb628836821910b75a5f7756f2d583ddf
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-10-11 17:15:41 -0500

    soundwire: intel: reset pm_runtime status during system resume
    The system resume does the entire bus re-initialization and brings it
    to full-power. If the device was pm_runtime suspended, there is no
    need to run the pm_runtime resume sequence after the system runtime.
    
    Follow the documentation from runtime_pm.rst, and conditionally
    disable, set_active and re-enable the device on system resume.
    
    Note that pm_runtime_suspended() is used instead of
    pm_runtime_status_suspended() so that we can deal with the case where
    pm_runtime is disabled.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1301,6 +1301,7 @@ static int intel_master_remove(struct sdw_master_device *md)
 
 static int intel_suspend(struct device *dev)
 {
+	struct sdw_master_device *md = dev_to_sdw_master_device(dev);
 	struct sdw_cdns *cdns = dev_get_drvdata(dev);
 	struct sdw_intel *sdw = cdns_to_intel(cdns);
 	int ret;
@@ -1308,6 +1309,20 @@ static int intel_suspend(struct device *dev)
 	if (cdns->bus.prop.hw_disabled) {
 		dev_dbg(dev, "SoundWire master %d is disabled, ignoring\n",
 			cdns->bus.link_id);
+		return 0;
+	}
+
+	if (pm_runtime_suspended(dev)) {
+		dev_dbg(dev,
+			"%s: pm_runtime status: suspended\n",
+			__func__);
+
+		/*
+		 * keep track of the state for the system resume, where
+		 * we will need to reset the pm_runtime status to active
+		 */
+		md->pm_runtime_suspended = true;
+
 		return 0;
 	}
 
@@ -1359,6 +1374,7 @@ static int intel_suspend_runtime(struct device *dev)
 
 static int intel_resume(struct device *dev)
 {
+	struct sdw_master_device *md = dev_to_sdw_master_device(dev);
 	struct sdw_cdns *cdns = dev_get_drvdata(dev);
 	struct sdw_intel *sdw = cdns_to_intel(cdns);
 	int ret;
@@ -1367,6 +1383,20 @@ static int intel_resume(struct device *dev)
 		dev_dbg(dev, "SoundWire master %d is disabled, ignoring\n",
 			cdns->bus.link_id);
 		return 0;
+	}
+
+	if (md->pm_runtime_suspended) {
+		dev_dbg(dev,
+			"%s: pm_runtime status was suspended, forcing active\n",
+			__func__);
+
+		/* follow required sequence from runtime_pm.rst */
+		pm_runtime_disable(dev);
+		pm_runtime_set_active(dev);
+		pm_runtime_mark_last_busy(dev);
+		pm_runtime_enable(dev);
+
+		md->pm_runtime_suspended = false;
 	}
 
 	ret = intel_init(sdw);
