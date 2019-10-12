d9603a255f158e4ad23747fa5d7ad4d520e771f3
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-10-12 12:32:57 -0500

    [HACK] add traces to debug aplay suspend/resume issue
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/sof/pm.c
+++ sound/soc/sof/pm.c
@@ -52,6 +52,8 @@ static int sof_resume(struct device *dev, bool runtime_resume)
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 	int ret;
 
+	dev_err(dev, "%s\n", __func__);
+
 	/* do nothing if dsp resume callbacks are not set */
 	if (!sof_ops(sdev)->resume || !sof_ops(sdev)->runtime_resume)
 		return 0;
@@ -124,6 +126,8 @@ static int sof_suspend(struct device *dev, bool runtime_suspend)
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 	int ret;
 
+	dev_err(dev, "%s\n", __func__);
+
 	/* do nothing if dsp suspend callback is not set */
 	if (!sof_ops(sdev)->suspend)
 		return 0;
@@ -180,6 +184,7 @@ static int sof_suspend(struct device *dev, bool runtime_suspend)
 
 int snd_sof_runtime_suspend(struct device *dev)
 {
+	dev_err(dev, "%s\n", __func__);
 	return sof_suspend(dev, true);
 }
 EXPORT_SYMBOL(snd_sof_runtime_suspend);
@@ -188,12 +193,14 @@ int snd_sof_runtime_idle(struct device *dev)
 {
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 
+	dev_err(dev, "%s\n", __func__);
 	return snd_sof_dsp_runtime_idle(sdev);
 }
 EXPORT_SYMBOL(snd_sof_runtime_idle);
 
 int snd_sof_runtime_resume(struct device *dev)
 {
+	dev_err(dev, "%s\n", __func__);
 	return sof_resume(dev, true);
 }
 EXPORT_SYMBOL(snd_sof_runtime_resume);
@@ -255,6 +262,8 @@ int snd_sof_resume(struct device *dev)
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 	int ret;
 
+	dev_err(dev, "%s\n", __func__);
+
 	if (snd_sof_dsp_d0i3_on_suspend(sdev)) {
 		/* resume from D0I3 */
 		dev_dbg(sdev->dev, "DSP will exit from D0i3...\n");
@@ -283,6 +292,8 @@ int snd_sof_suspend(struct device *dev)
 {
 	struct snd_sof_dev *sdev = dev_get_drvdata(dev);
 	int ret;
+
+	dev_err(dev, "%s\n", __func__);
 
 	if (snd_sof_dsp_d0i3_on_suspend(sdev)) {
 		/* suspend to D0i3 */
