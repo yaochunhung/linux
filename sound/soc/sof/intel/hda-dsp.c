bf721ae41ea9244a4481f621295d0582b8311b9b
Keyon Jie <yang.jie@linux.intel.com>
2019-10-19 03:31:57 +0800

    ASoC: SOF: Intel: hda-dsp: implement suspend/resume for S0ix<->S0 transition
    When the system is suspending to the S0ix state, we will enable the DMI
    L1 support, and enable waking up the systeam via IPC interrupt from the
    DSP.
    
    When the system is resuming from S0ix, the reversed sequence will be
    performed to restore the state before the S0ix entry.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- sound/soc/sof/intel/hda-dsp.c
+++ sound/soc/sof/intel/hda-dsp.c
@@ -453,6 +453,22 @@ static int hda_resume(struct snd_sof_dev *sdev, bool runtime_resume)
 
 int hda_dsp_resume(struct snd_sof_dev *sdev)
 {
+	struct sof_intel_hda_dev *hda = sdev->pdata->hw_pdata;
+	struct pci_dev *pci = to_pci_dev(sdev->dev);
+
+	if (sdev->s0_suspend) {
+		/* restore L1SEN state */
+		if (hda->l1_support_changed)
+			snd_sof_dsp_update_bits(sdev, HDA_DSP_HDA_BAR,
+						HDA_VS_INTEL_EM2,
+						HDA_VS_INTEL_EM2_L1SEN, 0);
+
+		/* restore and disable the system wakeup */
+		pci_restore_state(pci);
+		disable_irq_wake(pci->irq);
+		return 0;
+	}
+
 	/* init hda controller. DSP cores will be powered up during fw boot */
 	return hda_resume(sdev, false);
 }
@@ -484,8 +500,24 @@ int hda_dsp_runtime_suspend(struct snd_sof_dev *sdev)
 
 int hda_dsp_suspend(struct snd_sof_dev *sdev)
 {
+	struct sof_intel_hda_dev *hda = sdev->pdata->hw_pdata;
 	struct hdac_bus *bus = sof_to_bus(sdev);
+	struct pci_dev *pci = to_pci_dev(sdev->dev);
 	int ret;
+
+	if (sdev->s0_suspend) {
+		/* enable L1SEN to make sure the system can enter S0Ix */
+		hda->l1_support_changed =
+			snd_sof_dsp_update_bits(sdev, HDA_DSP_HDA_BAR,
+						HDA_VS_INTEL_EM2,
+						HDA_VS_INTEL_EM2_L1SEN,
+						HDA_VS_INTEL_EM2_L1SEN);
+
+		/* enable the system waking up via IPC IRQ */
+		enable_irq_wake(pci->irq);
+		pci_save_state(pci);
+		return 0;
+	}
 
 	/* stop hda controller and power dsp off */
 	ret = hda_suspend(sdev, false);
