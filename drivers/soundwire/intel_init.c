7ae3508c5d18e9f7b8fc1da690e7d77c5a7ff1a8
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-12-02 16:05:13 -0600

    soundwire: intel: add mutex to prevent concurrent access to SHIM registers
    Some of the SHIM registers exposed fields that are link specific, and
    in addition some of the power-related registers (SPA/CPA) take time to
    be updated. Uncontrolled access leads to timeouts or errors.
    
    Add a mutex at the controller level, shared by all links, so that all
    accesses to such registers are serialized, and follow a pattern of
    read-modify-write.
    
    GitHub issue: https://github.com/thesofproject/linux/issues/1555
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel_init.c
+++ drivers/soundwire/intel_init.c
@@ -222,6 +222,7 @@ static struct sdw_intel_ctx
 	ctx->mmio_base = res->mmio_base;
 	ctx->link_mask = res->link_mask;
 	ctx->handle = res->handle;
+	mutex_init(&ctx->shim_lock);
 
 	link = ctx->links;
 	link_mask = ctx->link_mask;
@@ -242,6 +243,7 @@ static struct sdw_intel_ctx
 		link->ops = res->ops;
 		link->dev = res->dev;
 		link->clock_stop_quirks = res->clock_stop_quirks;
+		link->shim_lock = &ctx->shim_lock;
 
 		init_completion(&link->probe_complete);
 
