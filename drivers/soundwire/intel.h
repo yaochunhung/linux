15057e1238122495cc07a598ddabab5490fe489f
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-01-31 15:42:49 -0600

    soundwire: intel: implement interfaces for SOF integration
    Rather than a plain-vanilla init/exit, this patch provides 3 steps in
    the initialization (ACPI scan, probe, startup) which makes it easier
    to detect platform support for SoundWire, allocate required resources
    as early as possible, and conversely help make the startup() funtionality
    lighter-weight with only hardware register setup.
    
    In addition, transition to the new interfaces needed for SOF
    integration.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.h
+++ drivers/soundwire/intel.h
@@ -7,7 +7,14 @@
 /**
  * struct sdw_intel_link_res - Soundwire Intel link resource structure,
  * typically populated by the controller driver.
+ *
+ * When used with platform_devices platdata, the data will be
+ * copied. Any information provided back to the parent needs to use
+ * the @parent_res indirection
+ *
+ * @parent_res: address of parent resources
  * @pdev: platform_device
+ * @probe_complete: completion utility to signal successful probe completion
  * @mmio_base: mmio base of SoundWire registers
  * @registers: Link IO registers base
  * @shim: Audio shim pointer
@@ -15,9 +22,13 @@
  * @irq: Interrupt line
  * @ops: Shim callback ops
  * @dev: device implementing hw_params and free callbacks
+ * @shim_lock: mutex to handle access to shared SHIM registers
+ * @clock_stop_quirks: mask defining requested behavior on pm_suspend
  */
 struct sdw_intel_link_res {
+	void *parent_res;
 	struct platform_device *pdev;
+	struct completion probe_complete;
 	void __iomem *mmio_base; /* not strictly needed, useful for debug */
 	void __iomem *registers;
 	void __iomem *shim;
@@ -25,6 +36,14 @@ struct sdw_intel_link_res {
 	int irq;
 	const struct sdw_intel_ops *ops;
 	struct device *dev;
+	struct mutex *shim_lock; /* protect shared registers */
+	u32 clock_stop_quirks;
 };
+
+#define SDW_INTEL_QUIRK_MASK_BUS_DISABLE      BIT(1)
+
+#define SDW_INTEL_MASTER_PROBE_TIMEOUT 2000
+
+int intel_master_startup(struct platform_device *pdev);
 
 #endif /* __SDW_INTEL_LOCAL_H */
