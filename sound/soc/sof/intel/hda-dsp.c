e3c714ba006eb1108fe4a5a37da985d9de094ae7
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2020-02-04 16:01:41 +0200

    ASoC: SOF: Intel: hda: fix ordering bug in resume flow
    When HDA controller is resumed from suspend, i915 HDMI/DP
    codec requires that following order of actions is kept:
    
     - i915 display power up and configuration of link params
     - hda link reset and setup
    
    Current SOF HDA code delegates display codec power control
    to the codec driver. This works most of the time, but in
    runtime PM sequences, the above constraint may be violated.
    On platforms where BIOS values for HDA link parameters do
    not match hardware reset defaults, this may lead to errors
    in HDA verb transactions after resume.
    
    Fix the issue by explicitly powering the display codec
    in the HDA controller runtime resume/suspend calls, thus
    ensuring correct ordering.
    
    Note that early powering of display was removed in
    commit 687ae9e287b3 ("ASoC: intel: skl: Fix display power regression").
    This change was also copied to the SOF driver. No failures
    have resulted as hardware default values for link parameters
    have worked out of the box. However with recent i915 driver
    changes like done in commit 87c1694533c9 ("drm/i915: save
    AUD_FREQ_CNTRL state at audio domain suspend"), this does not
    hold anymore and errors are hit.
    
    Cc: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- sound/soc/sof/intel/hda-dsp.c
+++ sound/soc/sof/intel/hda-dsp.c
@@ -595,6 +595,12 @@ static int hda_suspend(struct snd_sof_dev *sdev, bool runtime_suspend)
 		return ret;
 	}
 
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_HDA)
+	/* display codec can powered off after link reset */
+	if (HDA_IDISP_CODEC(bus->codec_mask))
+		hda_codec_i915_display_power(sdev, false);
+#endif
+
 	return 0;
 }
 
@@ -605,6 +611,12 @@ static int hda_resume(struct snd_sof_dev *sdev, bool runtime_resume)
 	struct hdac_ext_link *hlink = NULL;
 #endif
 	int ret;
+
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_HDA)
+	/* display codec must be powered before link reset */
+	if (HDA_IDISP_CODEC(bus->codec_mask))
+		hda_codec_i915_display_power(sdev, true);
+#endif
 
 	/*
 	 * clear TCSEL to clear playback on some HD Audio
