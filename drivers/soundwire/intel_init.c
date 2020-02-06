8cfe5c985706775d9342bc912e523590baed269e
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-02-06 15:15:24 -0600

    soundwire: intel: add definitions for clock stop quirks
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel_init.c
+++ drivers/soundwire/intel_init.c
@@ -247,6 +247,7 @@ static struct sdw_intel_ctx
 		link->dev = res->dev;
 		link->clock_stop_quirks = res->clock_stop_quirks;
 		link->shim_lock = &ctx->shim_lock;
+		link->shim_mask = &ctx->shim_mask;
 
 		md = sdw_master_device_add("intel-master",
 					   res->parent,
