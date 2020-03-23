6414beb244c222d9f9788e1bcbe249cf67e1dde7
Takashi Iwai <tiwai@suse.de>
2020-03-23 18:06:43 +0100

    ALSA: usb-audio: Avoid shoving a new stream into already registered device
    USB-audio driver registers the card and its devices at each probe of
    USB interface, while trying to append a USB substream into the empty
    PCM stream slot.  This works for most cases where the all PCM streams
    are declared in the single interface description.  However, when the
    device provides multiple individual interfaces, this may up with
    pushing a new stream into the existing snd_pcm object that has been
    already registered.  From the driver perspective, it's OK, but it
    doesn't work for PulseAudio and others because they manage in the card
    registration level, hence they'll miss this new device creation.
    
    This patch tries to warn such a too-late-appended stream, and also
    tries to put rather into a new snd_pcm object.
    
    If we get a report from a user about this, we may add it to an entry
    for snd_usb_registration_quirk().
    
    Link: https://lore.kernel.org/r/20200323170643.19181-2-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/usbaudio.h
+++ sound/usb/usbaudio.h
@@ -34,6 +34,7 @@ struct snd_usb_audio {
 	unsigned int txfr_quirk:1; /* Subframe boundaries on transfers */
 	unsigned int tx_length_quirk:1; /* Put length specifier in transfers */
 	unsigned int setup_fmt_after_resume_quirk:1; /* setup the format to interface after resume */
+	unsigned int pcm_devs_warned:1; /* warned for delayed PCM registrations */
 	int num_interfaces;
 	int num_suspended_intf;
 	int sample_rate_read_error;
