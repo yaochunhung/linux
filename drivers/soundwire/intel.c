9ada4d031c212903dd5635027dac0b73f01d2bde
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-06-25 16:03:51 -0500

    soundwire: intel: fix CONFIG_PM and CONFIG_PM_SLEEP confusion
    When CONFIG_PM_SLEEP is not defined, GCC throws compilation warnings:
    
    drivers/soundwire/intel.c:1816:12: warning: ‘intel_resume’ defined but
    not used [-Wunused-function]
     1816 | static int intel_resume(struct device *dev)
          |            ^~~~~~~~~~~~
    
    drivers/soundwire/intel.c:1697:12: warning: ‘intel_suspend’ defined
    but not used [-Wunused-function]
     1697 | static int intel_suspend(struct device *dev)
    
    Fix by adding the missing CONFIG_PM_SLEEP.
    
    Note that we could move code around and use only 2 ifdefs, but this
    will generate conflicts so let's do this when all the pm handling is
    merged.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1682,7 +1682,7 @@ int intel_master_process_wakeen_event(struct platform_device *pdev)
  * PM calls
  */
 
-#ifdef CONFIG_PM
+#ifdef CONFIG_PM_SLEEP
 
 static int intel_suspend(struct device *dev)
 {
@@ -1737,6 +1737,9 @@ static int intel_suspend(struct device *dev)
 
 	return 0;
 }
+#endif
+
+#ifdef CONFIG_PM
 
 static int intel_suspend_runtime(struct device *dev)
 {
@@ -1799,6 +1802,9 @@ static int intel_suspend_runtime(struct device *dev)
 
 	return ret;
 }
+#endif
+
+#ifdef CONFIG_PM_SLEEP
 
 static int intel_resume(struct device *dev)
 {
@@ -1892,6 +1898,9 @@ static int intel_resume(struct device *dev)
 
 	return ret;
 }
+#endif
+
+#ifdef CONFIG_PM
 
 static int intel_resume_runtime(struct device *dev)
 {
@@ -2067,7 +2076,6 @@ static int intel_resume_runtime(struct device *dev)
 
 	return ret;
 }
-
 #endif
 
 static const struct dev_pm_ops intel_pm = {
