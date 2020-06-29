3d919a8fa803d1cc446b6c65e2204740a7e3593f
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-06-29 18:14:27 -0500

    soundwire: bus: filter-out unwanted interrupt reports
    Unlike the traditional usage, in the SoundWire specification the
    interrupt masks only gate the propagation of an interrupt condition to
    the PING frame status. They do not gate the changes of the INT_STAT
    registers, which will happen regardless of the mask settings. See
    Figure 116 of the SoundWire 1.2 specification for an in-depth
    description of the interrupt model.
    
    When the bus driver reads the SCP_INT1_STAT register, it will retrieve
    all the interrupt status, including for the mask fields that were not
    explicitly set. For example, even if the PARITY mask is not set, the
    PARITY error status will be reported if an implementation-defined
    interrupt for jack detection is enabled and occurs.
    
    Filtering undesired interrupt reports and handling has to be
    implemented in software. This patch enables this filtering for the
    INT1_IMPL_DEF, PARITY and BUS_CLASH interrupt sources.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -1405,12 +1405,14 @@ static int sdw_handle_slave_alerts(struct sdw_slave *slave)
 		 * interrupt
 		 */
 		if (buf & SDW_SCP_INT1_PARITY) {
-			dev_err(&slave->dev, "Parity error detected\n");
+			if (slave->prop.scp_int1_mask & SDW_SCP_INT1_PARITY)
+				dev_err(&slave->dev, "Parity error detected\n");
 			clear |= SDW_SCP_INT1_PARITY;
 		}
 
 		if (buf & SDW_SCP_INT1_BUS_CLASH) {
-			dev_err(&slave->dev, "Bus clash error detected\n");
+			if (slave->prop.scp_int1_mask & SDW_SCP_INT1_BUS_CLASH)
+				dev_err(&slave->dev, "Bus clash detected\n");
 			clear |= SDW_SCP_INT1_BUS_CLASH;
 		}
 
@@ -1422,9 +1424,11 @@ static int sdw_handle_slave_alerts(struct sdw_slave *slave)
 		 */
 
 		if (buf & SDW_SCP_INT1_IMPL_DEF) {
-			dev_dbg(&slave->dev, "Slave impl defined interrupt\n");
+			if (slave->prop.scp_int1_mask & SDW_SCP_INT1_IMPL_DEF) {
+				dev_dbg(&slave->dev, "Slave impl defined interrupt\n");
+				slave_notify = true;
+			}
 			clear |= SDW_SCP_INT1_IMPL_DEF;
-			slave_notify = true;
 		}
 
 		/* the SDCA interrupts are cleared in the codec driver .interrupt_callback() */
