01536c2f0cbda27b875f1ffcd68e443c705f5114
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-08-27 10:24:35 -0500

    Revert "regmap: sdw: move to -EOPNOTSUPP"
    This reverts commit 4784b1fe608f05c847b96f6f1e7ed89e614212ff.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/base/regmap/regmap-sdw.c
+++ drivers/base/regmap/regmap-sdw.c
@@ -42,14 +42,14 @@ static int regmap_sdw_config_check(const struct regmap_config *config)
 {
 	/* All register are 8-bits wide as per MIPI Soundwire 1.0 Spec */
 	if (config->val_bits != 8)
-		return -EOPNOTSUPP;
+		return -ENOTSUPP;
 
 	/* Registers are 32 bits wide */
 	if (config->reg_bits != 32)
-		return -EOPNOTSUPP;
+		return -ENOTSUPP;
 
 	if (config->pad_bits != 0)
-		return -EOPNOTSUPP;
+		return -ENOTSUPP;
 
 	return 0;
 }
