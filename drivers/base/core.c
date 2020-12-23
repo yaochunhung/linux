d3e5a809dee8e88611aad73a55eb3b531467e2a2
Bard Liao <yung-chuan.liao@linux.intel.com>
2020-12-23 10:55:03 +0800

    device property: add description of fwnode cases
    There are only four valid fwnode cases which are
    - primary --> secondary --> -ENODEV
    - primary --> NULL
    - secondary --> -ENODEV
    - NULL
    
    dev->fwnode should be converted between the 4 cases above no matter
    how/when set_primary_fwnode() and set_secondary_fwnode() are called.
    Describe it in the code so people will keep it in mind.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -4259,6 +4259,12 @@ static inline bool fwnode_is_primary(struct fwnode_handle *fwnode)
  *
  * Set the device's firmware node pointer to @fwnode, but if a secondary
  * firmware node of the device is present, preserve it.
+ *
+ * Valid fwnode cases are:
+ *  - primary --> secondary --> -ENODEV
+ *  - primary --> NULL
+ *  - secondary --> -ENODEV
+ *  - NULL
  */
 void set_primary_fwnode(struct device *dev, struct fwnode_handle *fwnode)
 {
@@ -4277,6 +4283,7 @@ void set_primary_fwnode(struct device *dev, struct fwnode_handle *fwnode)
 	} else {
 		if (fwnode_is_primary(fn)) {
 			dev->fwnode = fn->secondary;
+			/* Set fn->secondary = NULL to keep fn as a primary fwnode */
 			if (!(parent && fn == parent->fwnode))
 				fn->secondary = NULL;
 		} else {
