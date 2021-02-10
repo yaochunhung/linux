076962ada58d834489cb6571d04a7fb4b8aa5bcc
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-02-10 15:44:41 -0600

    Revert "soundwire: bus: test read status"
    This reverts commit d3fceec0371de23974a224241d3ef5dc4aeab0ae.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -875,12 +875,8 @@ static int sdw_slave_clk_stop_prepare(struct sdw_slave *slave,
 		if (wake_en)
 			val |= SDW_SCP_SYSTEMCTRL_WAKE_UP_EN;
 	} else {
-		ret = sdw_read_no_pm(slave, SDW_SCP_SYSTEMCTRL);
-		if (ret < 0) {
-			dev_err(&slave->dev, "SDW_SCP_SYSTEMCTRL read failed:%d\n", ret);
-			return ret;
-		}
-		val = ret;
+		val = sdw_read_no_pm(slave, SDW_SCP_SYSTEMCTRL);
+
 		val &= ~(SDW_SCP_SYSTEMCTRL_CLK_STP_PREP);
 	}
 
@@ -901,10 +897,6 @@ static int sdw_bus_wait_for_clk_prep_deprep(struct sdw_bus *bus, u16 dev_num)
 	do {
 		val = sdw_bread_no_pm(bus, dev_num, SDW_SCP_STAT) &
 			SDW_SCP_STAT_CLK_STP_NF;
-		if (val < 0) {
-			dev_err(bus->dev, "SDW_SCP_STAT bread failed:%d\n", val);
-			return val;
-		}
 		if (!val) {
 			dev_info(bus->dev, "clock stop prep/de-prep done slave:%d",
 				 dev_num);
@@ -1272,11 +1264,6 @@ static int sdw_initialize_slave(struct sdw_slave *slave)
 	if (slave->bus->prop.quirks & SDW_MASTER_QUIRKS_CLEAR_INITIAL_CLASH) {
 		/* Clear bus clash interrupt before enabling interrupt mask */
 		status = sdw_read_no_pm(slave, SDW_SCP_INT1);
-		if (status < 0) {
-			dev_err(&slave->dev,
-				"SDW_SCP_INT1 read failed:%d\n", status);
-			return status;
-		}
 		if (status & SDW_SCP_INT1_BUS_CLASH) {
 			dev_warn(&slave->dev, "Bus clash detected before INT mask is enabled\n");
 			sdw_write_no_pm(slave, SDW_SCP_INT1, SDW_SCP_INT1_BUS_CLASH);
@@ -1286,11 +1273,6 @@ static int sdw_initialize_slave(struct sdw_slave *slave)
 	    !(slave->prop.quirks & SDW_SLAVE_QUIRKS_INVALID_INITIAL_PARITY)) {
 		/* Clear parity interrupt before enabling interrupt mask */
 		status = sdw_read_no_pm(slave, SDW_SCP_INT1);
-		if (status < 0) {
-			dev_err(&slave->dev,
-				"SDW_SCP_INT1 read failed:%d\n", status);
-			return status;
-		}
 		if (status & SDW_SCP_INT1_PARITY) {
 			dev_warn(&slave->dev, "PARITY error detected before INT mask is enabled\n");
 			sdw_write_no_pm(slave, SDW_SCP_INT1, SDW_SCP_INT1_PARITY);
