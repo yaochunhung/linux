af30aa1821fd5669dfc94d6260d12fdfbfc9a861
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-03-03 18:04:02 -0800

    ASoC: topology: change the complete op in snd_soc_tplg_ops to return int
    In the SOF driver, the operations performed in the complete callback
    can fail and therefore topology loading should return an error in
    such cases. So, change the signature of the complete op
    in struct snd_soc_tplg_ops to return an int to return the error.
    
    Also, amend the complete callback functions in the SOF driver and
    the SKL driver to conform with the new signature.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/intel/skylake/skl-topology.c
+++ sound/soc/intel/skylake/skl-topology.c
@@ -3610,7 +3610,7 @@ static int skl_manifest_load(struct snd_soc_component *cmpnt, int index,
 	return 0;
 }
 
-static void skl_tplg_complete(struct snd_soc_component *component)
+static int skl_tplg_complete(struct snd_soc_component *component)
 {
 	struct snd_soc_dobj *dobj;
 	struct snd_soc_acpi_mach *mach;
@@ -3619,7 +3619,7 @@ static void skl_tplg_complete(struct snd_soc_component *component)
 
 	val = kmalloc(sizeof(*val), GFP_KERNEL);
 	if (!val)
-		return;
+		return -ENOMEM;
 
 	mach = dev_get_platdata(component->card->dev);
 	list_for_each_entry(dobj, &component->dobj_list, list) {
@@ -3644,7 +3644,9 @@ static void skl_tplg_complete(struct snd_soc_component *component)
 			}
 		}
 	}
+
 	kfree(val);
+	return 0;
 }
 
 static struct snd_soc_tplg_ops skl_tplg_ops  = {
