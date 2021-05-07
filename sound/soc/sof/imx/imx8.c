54868e481d73199e25b17e941f69acef18dd07a3
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2021-05-07 11:09:16 +0300

    ASoC: SOF: imx: Do not initialize the snd_sof_dsp_ops.read64
    The read64 operation is not used by IMX along with other IO functions.
    No need to set it for the ops.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/imx/imx8.c
+++ sound/soc/sof/imx/imx8.c
@@ -418,9 +418,6 @@ struct snd_sof_dsp_ops sof_imx8_ops = {
 	.block_read	= sof_block_read,
 	.block_write	= sof_block_write,
 
-	/* Module IO */
-	.read64	= sof_io_read64,
-
 	/* ipc */
 	.send_msg	= imx8_send_msg,
 	.fw_ready	= sof_fw_ready,
@@ -466,9 +463,6 @@ struct snd_sof_dsp_ops sof_imx8x_ops = {
 	/* Block IO */
 	.block_read	= sof_block_read,
 	.block_write	= sof_block_write,
-
-	/* Module IO */
-	.read64	= sof_io_read64,
 
 	/* ipc */
 	.send_msg	= imx8_send_msg,
