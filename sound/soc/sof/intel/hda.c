0ac3416c35726757f356d0b37fdfea42af5f4184
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-07-29 13:51:52 -0500

    fixup! ASoC: SOF: Intel: hda: add new flags for DAI_CONFIG
    The DAI config->flags is an u8 type, which on some architectures leads
    to link issues such as
    
    ERROR: modpost: "__bad_cmpxchg" [sound/soc/sof/snd-sof.ko] undefined!
    ERROR: modpost: "__bad_cmpxchg" [sound/soc/sof/intel/snd-sof-intel-hda-common.ko] undefined!
    
    This is root-caused to the use of set_mask_bits(), which is completely
    overkill here. We don't need any sort of atomicity, just set a bit
    before sending an IPC.
    
    The implementation of set_mask_bits() should really detect that the
    data type is not supported, but that's a separate story. We can use a
    simpler macro instead.
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda.c
+++ sound/soc/sof/intel/hda.c
@@ -72,9 +72,7 @@ int hda_ctrl_dai_widget_setup(struct snd_soc_dapm_widget *w)
 	}
 
 	/* set HW_PARAMS flag */
-	set_mask_bits(&config->flags,
-		      SOF_DAI_CONFIG_FLAGS_MASK,
-		      SOF_DAI_CONFIG_FLAGS_HW_PARAMS);
+	config->flags = FIELD_PREP(SOF_DAI_CONFIG_FLAGS_MASK, SOF_DAI_CONFIG_FLAGS_HW_PARAMS);
 
 	/* send DAI_CONFIG IPC */
 	ret = sof_ipc_tx_message(sdev->ipc, config->hdr.cmd, config, config->hdr.size,
@@ -113,9 +111,7 @@ int hda_ctrl_dai_widget_free(struct snd_soc_dapm_widget *w)
 	config = &sof_dai->dai_config[sof_dai->current_config];
 
 	/* set HW_FREE flag */
-	set_mask_bits(&config->flags,
-		      SOF_DAI_CONFIG_FLAGS_MASK,
-		      SOF_DAI_CONFIG_FLAGS_HW_FREE);
+	config->flags = FIELD_PREP(SOF_DAI_CONFIG_FLAGS_MASK, SOF_DAI_CONFIG_FLAGS_HW_FREE);
 
 	ret = sof_ipc_tx_message(sdev->ipc, config->hdr.cmd, config, config->hdr.size,
 				  &reply, sizeof(reply));
--- sound/soc/sof/sof-audio.c
+++ sound/soc/sof/sof-audio.c
@@ -8,6 +8,7 @@
 // Author: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
 //
 
+#include <linux/bitfield.h>
 #include "sof-audio.h"
 #include "ops.h"
 
@@ -56,9 +57,7 @@ static int sof_dai_config_setup(struct snd_sof_dev *sdev, struct snd_sof_dai *da
 	}
 
 	/* set NONE flag to clear all previous settings */
-	set_mask_bits(&config->flags,
-		      SOF_DAI_CONFIG_FLAGS_MASK,
-		      SOF_DAI_CONFIG_FLAGS_NONE);
+	config->flags = FIELD_PREP(SOF_DAI_CONFIG_FLAGS_MASK, SOF_DAI_CONFIG_FLAGS_NONE);
 
 	ret = sof_ipc_tx_message(sdev->ipc, config->hdr.cmd, config, config->hdr.size,
 				 &reply, sizeof(reply));
