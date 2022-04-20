078f50d9298d7309b21fca7dd263e08d3d17d8a9
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2022-04-20 15:00:11 +0300

    fixup! ASoC: SOF: Make sof_suspend/resume IPC agnostic
    mark ctx_store and ctx_restore as Optional.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/sof-priv.h
+++ sound/soc/sof/sof-priv.h
@@ -376,8 +376,8 @@ struct sof_ipc_fw_tracing_ops {
 
 /**
  * struct sof_ipc_pm_ops - IPC-specific PM ops
- * @ctx_save:		Function pointer for context save
- * @ctx_restore:	Function pointer for context restore
+ * @ctx_save:		Optional function pointer for context save
+ * @ctx_restore:	Optional function pointer for context restore
  * @set_core_state:	Function pointer for turning on/off a DSP core
  */
 struct sof_ipc_pm_ops {
